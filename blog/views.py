1360ca09a0eca3222ae263effc8322eb52dc84ff
maingate_choi <ttbj123@gmail.com>
2016-06-08 16:43:00 +0900

    카테고리페이지와 샵 페이지 링크

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Category, Shop, Review
 from .forms import CategoryForm
 
@@ -15,8 +15,7 @@ def category_detail(request, c_pk):
 
 def shop_detail(request, c_pk, s_pk):
 	category = Category.objects.get(pk=c_pk)
-
-	shop= Shop.obejects.filter(category = category)
+	shop = Category.objects.get(pk=s_pk).filter(category=category)
 	review_list = Review.obejects.all().fileter(shop=shop)
 	return render(request, 'blog/shop_detail.html', {'shop':shop, 'review_list':review_list})
 
@@ -29,3 +28,18 @@ def category_new(request):
 	else : 
 		form = CategoryForm()
 	return render(request, 'blog/category_form.html', {'form':form,})
+
+def category_edit(request, c_pk):
+	category = get_object_or_404(Category, pk = c_pk)
+	if request.method == 'POST':
+			form = CategoryForm(request.POST, instance=category)
+			if form.is_valid():
+				form.save()
+
+			return redirect('blog:category_detail', c_pk)
+	else:
+			form = CategoryForm(instance=category)
+	return render(request,'blog/category_form.html', {'form':form,})
+
+
+
