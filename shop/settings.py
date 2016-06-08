6397b79337b4296f671943ce26342e6e350357da
maingate_choi <ttbj123@gmail.com>
2016-06-08 15:47:14 +0900

    accounts app 생성

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accoutns',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = '/'
--- shop/urls.py
+++ shop/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include("blog.urls", namespace='blog')),
     url(r'^$/', lambda request:redirect('/blog')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
