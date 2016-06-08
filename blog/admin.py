c092340ab838cad82a87581f200656e1c43e6000
maingate_choi <ttbj123@gmail.com>
2016-06-08 14:43:01 +0900

    category, shop, review-admin 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Category, Shop, Review
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'created_at']
+
+class ShopAdmin(admin.ModelAdmin):
+	list_display = ['category', 'shop_name', 'created_at']
+
+class ReviewAdmin(admin.ModelAdmin):
+	list_display = ['shop', 'user', 'comment']
+
+admin.site.register(Category, CategoryAdmin, Shop, ShopAdmin, Review, ReviewAdmin)
